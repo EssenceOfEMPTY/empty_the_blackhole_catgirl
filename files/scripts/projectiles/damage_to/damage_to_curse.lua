4e835ffeb28fcd771c1d886c132239002576dbd2
EssenceOfEMPTY <1642164812@qq.com>
2026-07-05 23:38:09 +0800

    v1.9 - oh no

--- files/scripts/projectiles/damage_to/damage_to_curse.lua
+++ files/scripts/projectiles/damage_to/damage_to_curse.lua
@@ -1,3 +1,3 @@
 dofile_once( 'mods/empty_the_blackhole_catgirl/files/scripts/empty/empty_utility.lua' )
 
-damage_to( get_root_entity( ), 'curse', 0.5, 1 )
+damage_to( get_root_entity( ), 'curse', 0.5, 1 / get_scale( ) )
